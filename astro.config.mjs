99973c85506c2e98e8c347c3f788ba850083f7ad
Jarva <4622609+Jarva@users.noreply.github.com>
2024-03-25 22:56:35 +0000

    fix: change outdir

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import purgecss from "astro-purgecss";
 
 // https://astro.build/config
 export default defineConfig({
-  // output: 'server',
+  outDir: 'public',
   site: 'https://ars.guide',
   integrations: [mdx(), react(), purgecss()],
 });
