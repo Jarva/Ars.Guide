420fdb037f24bce3d2b0e21e32a23950da600904
Jarva <4622609+Jarva@users.noreply.github.com>
2024-03-26 22:11:49 +0000

    feat: add header links

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,32 @@ import { defineConfig } from 'astro/config';
 import mdx from "@astrojs/mdx";
 import react from "@astrojs/react";
 import purgecss from "astro-purgecss";
+import {visit} from 'unist-util-visit'
 
 import expressiveCode from "astro-expressive-code";
+import rehypeSlug from 'rehype-slug'
 // import { pluginLineNumbers } from '@expressive-code/plugin-line-numbers'
-import rehypeAutolinkHeadings from 'rehype-autolink-headings'
+
+const addHeaderLinks = () => {
+  return (tree) => {
+    visit(tree, 'element', (node) => {
+      if (['h2', 'h3', 'h4', 'h5'].includes(node.tagName) && node.properties.id) {
+        node.children.push({
+          type: 'element',
+          tagName: 'a',
+          properties: {
+            class: 'anchor',
+            'aria-hidden': true,
+            href: '#' + node.properties.id
+          },
+          children: [
+            { type: 'text', value: '#' }
+          ]
+        })
+      }
+    })
+  }
+}
 
 // https://astro.build/config
 export default defineConfig({
@@ -20,6 +42,6 @@ export default defineConfig({
     },
   }), mdx(), react(), purgecss()],
   markdown: {
-    remarkPlugins: [rehypeAutolinkHeadings]
+    rehypePlugins: [rehypeSlug, addHeaderLinks]
   }
 });
