a82b3b6bd3a85669a6c5de9644bbce7ff013818a
Jarva <4622609+Jarva@users.noreply.github.com>
2024-05-06 21:35:52 +0100

    feat: refactor POST api

---
+++ src/pages/api/submit.ts
@@ -0,0 +1,10 @@
+import type { APIContext } from 'astro';
+
+export async function POST({ request }: APIContext) {
+    const body = await request.formData();
+  
+    const obj = Object.fromEntries(body);
+    console.log("OBJ", JSON.stringify(obj));
+
+    return new Response("OK", { status: 200 });
+}
