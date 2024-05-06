575364f5cf6d5c1d0d8d9c9c19b4ef9de46b0273
Jarva <4622609+Jarva@users.noreply.github.com>
2024-05-06 23:30:43 +0100

    fix: make worker_threads external

--- astro.config.mjs
+++ astro.config.mjs
@@ -60,7 +60,7 @@ export default defineConfig({
   }),
   vite: {
     ssr: {
-      external: ['node:events', 'node:process'],
+      external: ['node:events', 'node:process', 'node:worker_threads'],
     },
   },
 });
