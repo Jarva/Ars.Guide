9c23ef356f804c7671d0f2f70ccc0117fe147109
Jarva <4622609+Jarva@users.noreply.github.com>
2024-05-27 22:27:19 +0100

    fix: remove sentry middleware

--- functions/_middleware.ts
+++
@@ -1,8 +0,0 @@
-import type { PagesFunction } from '@cloudflare/workers-types'
-import sentryPlugin from "@cloudflare/pages-plugin-sentry";
-
-export const onRequest: PagesFunction<{
-  SENTRY_DSN: string;
-}> = (context) => {
-  return sentryPlugin({ dsn: context.env.SENTRY_DSN })(context);
-};
