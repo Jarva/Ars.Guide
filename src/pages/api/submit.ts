7e245ca7b17549665c1003933b503a339b1d4ce0
Jarva <4622609+Jarva@users.noreply.github.com>
2024-05-27 17:34:55 +0100

    feat: add spell display page

--- src/pages/api/submit.ts
+++ src/pages/api/submit.ts
@@ -1,6 +1,6 @@
 import type { APIContext } from 'astro';
 import { EmbedBuilder } from "@discordjs/builders";
-import { spellFormSchema } from '../../utils/spell-form';
+import { addonMap, glyphMap, spellFormSchema, transformMultiSelect, versionMap } from '../../utils/spell-form';
 
 export const prerender = false;
 
@@ -38,11 +38,11 @@ export async function POST(context: CloudflareContext) {
         .addFields([
             { name: "Author", value: body.author, inline: true },
             { name: "Spell", value: body.spell, inline: true },
-            { name: "Glyphs", value: body.glyphs.join(" ➝ ") },
+            { name: "Glyphs", value: transformMultiSelect(glyphMap)(body.glyphs).join(" ➝ ") },
             { name: "Category", value: body.category, inline: true },
-            { name: "Addons", value: body.addons.join(", "), inline: true },
+            { name: "Addons", value: transformMultiSelect(addonMap)(body.addons).join(", "), inline: true },
             { name: "Description", value: body.description },
-            { name: "Versions", value: body.versions.join(", "), inline: true },
+            { name: "Versions", value: body.versions.split(",").join(", "), inline: true },
             { name: "Requires Infinite Spell?", value: body.infinite ? "Yes" : "No", inline: true },
         ])
         .setTimestamp();
@@ -56,13 +56,44 @@ export async function POST(context: CloudflareContext) {
             username: "Source Librarian",
             avatar_url: "https://cdn.discordapp.com/avatars/1235017501419765800/ff05eb9f01601892dd7b083dad16798d.webp?size=4096",
             embeds: [embed.toJSON()],
+            allowed_mentions: false
         })
     });
+
+    const adminContent = [
+        "```markdown",
+        `## ${body.spell}`,
+        "<Spell",
+        `    author='${body.author}'`,
+        `    glyphs={${JSON.stringify(body.glyphs.split(","))}}`,
+        `    description='${body.description}'`,
+        `    versions={${JSON.stringify(body.versions.split(","))}}`,
+        `/>`,
+        "```"
+    ]
 
     if (!res.ok) {
         const json = await res.json();
         console.error("Discord Response", json);
     }
-    
+
+    const adminRes = await fetch(env.ADMIN_WEBHOOK_URL, {
+        method: "POST",
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            username: "Source Librarian",
+            avatar_url: "https://cdn.discordapp.com/avatars/1235017501419765800/ff05eb9f01601892dd7b083dad16798d.webp?size=4096",
+            content: adminContent.join("\n"),
+            allowed_mentions: false
+        })
+    });
+
+    if (!adminRes.ok) {
+        const json = await res.json();
+        console.error("Discord Response", json);
+    }
+
     return Response.redirect(url.origin, 303);
 }
