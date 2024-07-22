3ed4eb327d1419a0591e3139649abb01071cd534
clcment446 <125682659+clcment446@users.noreply.github.com>
2024-07-22 13:14:59 +0200

    things idk

---
+++ src/utils/spell-compendium/data/glyphs/ars_trinkets.ts
@@ -0,0 +1,22 @@
+import {t} from "../../../text.ts";
+
+export default {
+    "air_sword": t("Air Sword"),
+    "devour": t("Devour"),
+    "devour_soul": t("Devour Soul"),
+    "filter_self": t("Filter is Self"),
+    "filter_not_self": t("Filter is Not Self"),
+    "soul_inspector": t("Air Sword"),
+    "mana_bomb": t("Spirit Bomb"),
+    "exp_sac": t("Corrupted Blessing"),
+    "sacrifice": t("Sacrifice Health"),
+    "shadow_veil": t("Shadow Veil"),
+    "sound_boom": t("Sonic Boom"),
+    "sun_flare": t("Sun Flare"),
+    "water_spear": t("Water Spear"),
+    "aura":t("Aura")
+    "missile":t("Missile")
+    "overhead":t("Overhead"),
+    "propagate_missile": t("Propagate Missile"),
+    "propagate_overhead" :t("Propagate Overhead")
+}
